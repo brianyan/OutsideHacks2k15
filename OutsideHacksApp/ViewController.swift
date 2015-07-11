ceabd33c82e83c62917076f6c26aa75033b6d74a
Neil <neiluser@yahoo.com>
2015-07-11 14:02:55 -0700

    Added MapKit API

--- OutsideHacksApp/ViewController.swift
+++ OutsideHacksApp/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MKView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let initialLocation = MKView.userLocation
+        let regionRadius: CLLocationDistance = 1
+        func centerMap( location: CLLocation ){
+            let 
+        }
     }
 
     override func didReceiveMemoryWarning() {
