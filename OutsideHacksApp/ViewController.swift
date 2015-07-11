06b2c7ed34ed65a2eac0f04083659b2bdd7da007
Neil <neiluser@yahoo.com>
2015-07-11 13:12:10 -0700

    First push

---
+++ OutsideHacksApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OutsideHacksApp
+//
+//  Created by Neil Tu on 7/11/15.
+//  Copyright (c) 2015 icebrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
