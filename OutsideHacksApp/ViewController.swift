45f22b80fac275387f0c54e777869d95402a39f2
Neil <neiluser@yahoo.com>
2015-07-11 16:43:00 -0700

    Centered the map

--- OutsideHacksApp/ViewController.swift
+++ OutsideHacksApp/ViewController.swift
@@ -8,25 +8,37 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate{
     @IBOutlet weak var MKView: MKMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let initialLocation = MKView.userLocation
-        let regionRadius: CLLocationDistance = 1
-        func centerMap( location: CLLocation ){
-            let 
-        }
+        MKView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus){
+        MKView.showsUserLocation = (status == .AuthorizedAlways)
+    }
+    
+    func mapView(mapView: MKMapView!, didUpdateUserLocation userLocation: MKUserLocation!) {
+        if let coordinate = MKView.userLocation.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(coordinate, 500, 500)
+            MKView.setRegion(region, animated: true)
+        }
+    }
 
 }
 
