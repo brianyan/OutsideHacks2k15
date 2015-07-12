78474cf1166888d84461901bb3b8554598cfc88b
alanguo5520 <alanguo@Alans-MacBook-Pro.local>
2015-07-11 20:06:24 -0700

    Added distance tracking.

--- OutsideHacksApp/ViewController.swift
+++ OutsideHacksApp/ViewController.swift
@@ -12,8 +12,14 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate{
     var droppedPin = false
+    var firstDistance = false
+    
     @IBOutlet weak var MKView: MKMapView!
     @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Distance: UILabel!
+    
+    
+    
     
     let locationManager = CLLocationManager()
     
@@ -35,31 +41,67 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus){
         MKView.showsUserLocation = (status == .AuthorizedAlways)
     }
-    
+    var pointAnnotation:MKPointAnnotation = MKPointAnnotation()
     @IBAction func DropPin(sender: AnyObject) {
         if droppedPin{
             Button1.setTitle("Drop Pin", forState: UIControlState.Normal)
+            firstDistance = false
+            Distance.text = "N/A"
             droppedPin = false
             MKView.removeAnnotations(MKView.annotations)
+            
         }
         else{
             Button1.setTitle("Clear Pin", forState: UIControlState.Normal)
             droppedPin = true
-            var pointAnnotation:MKPointAnnotation = MKPointAnnotation()
+       
             pointAnnotation.coordinate = MKView.userLocation.location.coordinate
             pointAnnotation.title = "Your car"
             MKView.addAnnotation(pointAnnotation)
+            firstDistance = true
+            updateDistance()
         }
     }
-    
-    func mapView(theMapView: MKMAPView 
-    
+
     func mapView(mapView: MKMapView!, didUpdateUserLocation userLocation: MKUserLocation!) {
         if let coordinate = MKView.userLocation.location?.coordinate {
             let region = MKCoordinateRegionMakeWithDistance(coordinate, 500, 500)
             MKView.setRegion(region, animated: true)
+            updateDistance()
+            
+        }
+    }
+    func updateDistance() {
+        if firstDistance {
+            var request = MKDirectionsRequest()
+            var source = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2DMake(MKView.userLocation.location.coordinate.latitude, MKView.userLocation.location.coordinate.longitude), addressDictionary: nil))
+            var destination = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2DMake(pointAnnotation.coordinate.latitude, pointAnnotation.coordinate.longitude), addressDictionary: nil))
+            
+            // source and destination are the relevant MKMapItems
+            request.setSource(source)
+            request.setDestination(destination)
+            
+            // Specify the transportation type
+            request.transportType = MKDirectionsTransportType.Automobile;
+            
+            // If you're open to getting more than one route,
+            // requestsAlternateRoutes = true; else requestsAlternateRoutes = false;
+            request.requestsAlternateRoutes = false
+            
+            var directions = MKDirections(request: request)
+            
+            directions.calculateDirectionsWithCompletionHandler{(response, error) -> Void in
+
+                self.Distance.text = response.routes.first?.distance.description
+            }
         }
     }
     
+    
+    
+    
 }
+//The MKUserLocation class defines a specific type of annotation that identifies the user’s current location.
+
+
 
